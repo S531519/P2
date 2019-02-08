c393d3b33162925aad37cffb785c2c25f820140f
Yashwanth Reddy Bommineni <S531519@nwmissouri.edu>
2019-02-08 16:20:38 -0600

    Added test cases

--- tests/UnitTests/UnitTest1.cs
+++ tests/UnitTests/UnitTest1.cs
@@ -8,23 +8,39 @@ namespace UnitTests
         [Fact]
         public void Test1()
         {
-
+            var outcome = "odd";
+            var a = 5;
+            var actual = EvenorOdd(5);
+            Assert.Equal(outcome, actual);
         }
-        int EvenorOdd(int val)
+        [Theory]
+        [InlineData(4, "even")]
+        public void Test2(int a, String b)
         {
+            Assert.Equal(b, EvenorOdd(a));
+        }
+        String EvenorOdd(int val)
+        {
+            var str =" ";
             
             if (val == 0)
             {
-                sol.value = "neither even nor odd";
+                str = "neither even nor odd";
             }
             else if (val % 2 == 0)
             {
-                sol.value = "even";
+               str = "even";
+            }
+            else if (val < 0)
+            {
+                str = "Negative number";
             }
             else
             {
-                sol.value = "odd";
+                str = "odd";
             }
+            return str;
+            
         }
     }
 }
